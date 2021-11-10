1edfe61ec9ebb2cacb6d6abb0e5fae8a8772386f
Wolverine-Blues <tjbyers12@gmail.com>
2021-11-10 15:35:14 -0500

    javscript work

--- script.js
+++ script.js
@@ -9,13 +9,29 @@ $(".navicon").on("click", function() {
 });
 });
 
-///// Form Data/////
 function formFunct()
-{
-    {var bandData= document.getElementById("bandname").value;}
+    {
+    if(document.getElementById("bandname").value.length == 0)
+        {alert("Band Name is invalid")}
+    if(document.getElementById("email").value.length == 0)
+        {alert("Email is invalid")}
+    
+    else{
+    {var bandData= document.getElementById("bandname").value;} //let doesn't work to declare variable here for some reason
     {var emailData= document.getElementById("email").value;}
     {var fieldData= document.getElementById("field").value;}
 
     alert("Thanks for your interest, " +bandData +". I'll be in touch with you soon at " +emailData)
+    }
 }
+
+// function formFunct()
+// {
+//     {var bandData= document.getElementById("bandname").value;} //let doesn't work to declare variable here for some reason
+//     {var emailData= document.getElementById("email").value;}
+//     {var fieldData= document.getElementById("field").value;}
+
+//     alert("Thanks for your interest, " +bandData +". I'll be in touch with you soon at " +emailData)
+// }
+
 
