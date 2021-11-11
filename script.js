8c653808a153a0261330dc16d4f915f6c25e040a
Wolverine-Blues <tjbyers12@gmail.com>
2021-11-11 15:58:08 -0500

    form validation

--- script.js
+++ script.js
@@ -1,15 +1,37 @@
 /////               //////
 ///// Navbar toggle//////
 /////              /////
-$(function() {
+  $(function() {
+
 $(".navicon").on("click", function() {
-    if ($(".navitem").hasClass("show")) {
+    if ($(".navitem").hasClass("show")) 
+    {
         $(".navitem").removeClass("show");
-    } else {
+    } 
+    else 
+    {
         $(".navitem").addClass("show");
     }
 });
-});
+});     
+
+
+//commented code below kept breaking in browser but rewriting it in
+//jquery above fixed it. keeping this just in case I can get it to work with
+//vanilla js
+
+// let navItem = document.getElementsById("navitem");
+// let navIcon = document.getElementsById("navicon");
+// navIcon.addEventListener("click", () => {
+//     if (navItem.classList.contains("show")){
+//         navItem.classList.remove("show");
+//     }
+//     else {
+//         (navItem.classList.add("show"));
+//     }
+// }
+// )
+
 
 /////                       /////
 ///// Form email validation /////
@@ -20,16 +42,16 @@ function formValidate()
 {var emailData= document.getElementById("email").value};
 {var mailFormat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;}
 
-if(emailData.match(mailFormat))
-{
-alert("Thanks for your interest, " +bandData +". I'll be in touch with you soon at " +emailData);
-return true;
-}
+    if(emailData.match(mailFormat))
+    {
+        alert("Thanks for your interest, " +bandData +". I'll be in touch with you soon at " +emailData);
+        return true;
+    }
 
-else
-{
-alert("The email address was invalid");
-return false;
-}
+    else
+    {
+    alert("The email address was invalid");
+    return false;
+    }
 }
 
